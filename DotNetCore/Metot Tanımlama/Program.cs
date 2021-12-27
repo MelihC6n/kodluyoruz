3a6712bb1ccf2acb09ad6f5b9b8c8050c19a5768
Melih Can Akgunes <melihcanakgunes@gmail.com>
2021-12-27 22:19:19 +0300

    Create Program.cs

---
+++ DotNetCore/Metot Tanımlama/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5;
+            int b=5;
+            Console.WriteLine(Topla(a,b));
+            Console.WriteLine(a+" "+b);
+            metotlar ornek=new metotlar();
+            Console.WriteLine(ornek.refliMetot(ref a, ref b));
+            Console.WriteLine(a+" "+b);
+
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return deger1+deger2;
+        }
+
+    }
+
+    class metotlar
+    {
+        public int refliMetot(ref int deger1,ref int deger2)
+        {
+            deger1+=5;
+            deger2+=5;
+            return deger1+deger2;
+        }
+    }
+}
