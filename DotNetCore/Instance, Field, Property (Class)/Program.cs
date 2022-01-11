f395e08b170c49a7378b9d1b0f828c58ff213faf
Melih Can Akgunes <melihcanakgunes@gmail.com>
2022-01-11 21:58:18 +0300

    Create Program.cs

---
+++ DotNetCore/Instance, Field, Property (Class)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Instance__Field__Property__Class_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan = new Calisan()
+            {
+                Ad="Ayşe",
+                Soyad="Kuş",
+                No=541651,
+                Departman="Öyle Böyle"
+            };
+
+            calisan.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Calışanın adı = {0}",Ad);
+            Console.WriteLine("Calışanın Soyadı = {0}",Soyad);
+            Console.WriteLine("Calışanın No = {0}",No);
+            Console.WriteLine("Calışanın Departman = {0}",Departman);
+        }
+    }
+}
